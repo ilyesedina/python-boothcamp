f5e0d51b55313ad741b80cd2e707caaa84a05164
Edina Ilyes <edina.ilyes@LEGO.com>
2025-12-18 16:57:00 +0100

    day one challange

---
+++ day1/main.py
@@ -0,0 +1,10 @@
+# Band Name Generator
+# This program generates a band name based on user input.
+
+print("Welcome to the Band Name Generator.")
+city = input("What's the name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+
+bandName = city + " " + pet
+
+print ("Your band name is '" + bandName + "'!")
