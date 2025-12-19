4994640f5f941fbd3e5c13b2fd29a89e9b14acd0
Edina Ilyes <edina.ilyes@LEGO.com>
2025-12-19 05:02:11 +0100

    adding print statements to day 2

---
+++ day2/main.py
@@ -0,0 +1,6 @@
+# Tip calculator
+
+print("Welcome to the tip calculator! ")
+bill = input("What was the total bill? $ ")
+tip = input("What percentage tip would you like to give? 10, 12, or 15? ")
+people = input("How many people are splitting the bill? ")
