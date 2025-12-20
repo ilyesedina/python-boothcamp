35b0d42952c56ab1eae8cc330e7c069003d1ec15
Edina Ilyes <edina.ilyes@LEGO.com>
2025-12-20 12:28:47 +0100

    bill calculator

--- day2/main.py
+++ day2/main.py
@@ -1,6 +1,12 @@
 # Tip calculator
 
-print("Welcome to the tip calculator! ")
-bill = input("What was the total bill? $ ")
-tip = input("What percentage tip would you like to give? 10, 12, or 15? ")
-people = input("How many people are splitting the bill? ")
+print("Welcome to the tip calculator!")
+bill = float(input("What was the total bill? $"))
+tip_percent = float(input("What percentage tip would you like to give? 10, 12, or 15? "))
+people = int(input("How many people are splitting the bill? "))
+
+tip = bill * (tip_percent / 100)
+total_bill = bill + tip
+amount_per_person = total_bill / people
+
+print(f"Each person should pay: ${amount_per_person:.2f}")
