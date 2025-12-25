3623265ed1b33b5d8f4412797626558b0f7762ca
Edina Ilyes <edina.ilyes@LEGO.com>
2025-12-25 21:43:13 +0100

    day 4

---
+++ day4/lesson.py
@@ -0,0 +1,22 @@
+import random
+# import my_module
+
+# random_intiger = random.randint(1, 10)
+# print("Random Integer between 1 and 10:", random_intiger)
+
+# print("My favorite number is:", my_module.my_favorite_number)
+
+# random_number_0_to_1 = random.random()
+# print("Random number between 0 and 1:", random_number_0_to_1)
+
+# random_float_1_to_10 = random.uniform(1, 10)
+# print("Random float between 1 and 10:", random_float_1_to_10)
+
+# random_choice = random.choice(['heads', 'tails'])
+# print("Random choice from list:", random_choice)
+
+random_head_tail = random.randomint(0, 1)
+if random_head_tail == 0:
+    print("heads")
+else:
+    print("tails")
