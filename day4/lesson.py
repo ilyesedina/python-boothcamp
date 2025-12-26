cbcb2ec6d4072b21701e5f80c8273af1964cc09a
Edina Ilyes <edina.ilyes@LEGO.com>
2025-12-26 20:39:33 +0100

    lesson

--- day4/lesson.py
+++ day4/lesson.py
@@ -15,8 +15,43 @@ import random
 # random_choice = random.choice(['heads', 'tails'])
 # print("Random choice from list:", random_choice)
 
-random_head_tail = random.randomint(0, 1)
-if random_head_tail == 0:
-    print("heads")
-else:
-    print("tails")
+# random_head_tail = random.randomint(0, 1)
+# if random_head_tail == 0:
+#    print("heads")
+# else:
+#    print("tails")
+
+stat_of_america = [
+    "Delaware", "Pennsylvania", "New Jersey", "Georgia", "Connecticut",
+    "Massachusetts", "Maryland", "South Carolina", "New Hampshire", "Virginia",
+    "New York", "North Carolina", "Rhode Island", "Vermont", "Kentucky",
+    "Tennessee", "Ohio", "Louisiana", "Indiana", "Mississippi",
+    "Illinois", "Alabama", "Maine", "Missouri", "Arkansas",
+    "Michigan", "Florida", "Texas", "Iowa", "Wisconsin",
+    "California", "Minnesota", "Oregon", "Kansas", "West Virginia",
+    "Nevada", "Nebraska", "Colorado", "North Dakota", "South Dakota",
+    "Montana", "Washington", "Idaho", "Wyoming", "Utah",
+    "Oklahoma", "New Mexico", "Arizona", "Alaska", "Hawaii"
+]
+
+# stat_of_america.append("Angelaland")
+stat_of_america.extend(["Angelaland", "New Angelaland"])
+print(stat_of_america[-1])
+
+friends = ["Alice", "Bob", "Charlie", "David"]
+
+# option 1
+random_friend = random.choice(friends)
+print("Random friend selected:", random_friend)
+
+# print(len(stat_of_america))
+random_index_state = len(stat_of_america) # 50 -> 49
+print(stat_of_america[random_index_state - 1])
+
+#dirty_dozen = ["Strawberries", "Spinach", "Kale", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears", "Tomatoes", "Celery", "Potatoes" ]
+
+fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears", "Tomatoes"]
+vegetables = ["Spinach", "Kale", "Celery", "Potatoes"]
+
+dirty_dozen = [fruits, vegetables]
+print(dirty_dozen)
