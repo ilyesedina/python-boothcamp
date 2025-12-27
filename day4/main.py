606b3cd816e7462a33d6517412d5dc8d1765d84b
Edina Ilyes <edina.ilyes@LEGO.com>
2025-12-27 14:56:42 +0100

    Rock papers scissors game

--- day4/main.py
+++ day4/main.py
@@ -10,6 +10,16 @@ computer_choice = random.choice(options)
 print(f"You chose: {user_choice}")
 print(f"Computer chose: {computer_choice}")
 
+# Logic to determine the winner
+if user_choice == computer_choice:
+    print("It's a tie!")
+elif (user_choice == "rock" and computer_choice == "scissors") or \
+     (user_choice == "paper" and computer_choice == "rock") or \
+     (user_choice == "scissors" and computer_choice == "paper"):
+    print("You win!")
+else:
+    print("Computer wins!")
+
 #prit out choices drawing ascii art
 rock_art = '''
     _______
@@ -38,13 +48,3 @@ scissors_art = '''
 art_options = [rock_art, paper_art, scissors_art]
 print(art_options[int(user_input)])
 print(art_options[options.index(computer_choice)])
-
-# Logic to determine the winner
-if user_choice == computer_choice:
-    print("It's a tie!")
-elif (user_choice == "rock" and computer_choice == "scissors") or \
-     (user_choice == "paper" and computer_choice == "rock") or \
-     (user_choice == "scissors" and computer_choice == "paper"):
-    print("You win!")
-else:
-    print("Computer wins!")
