27dd56ad106e657de4cdf7977ba81c6921450d3e
Edina Ilyes <edina.ilyes@LEGO.com>
2025-12-30 20:55:03 +0100

    Pick out the top grade

--- day5/lesson.py
+++ day5/lesson.py
@@ -2,4 +2,28 @@
 fruits = ["apple", "banana", "cherry"]
 for fruit in fruits:
     print(fruit)
-    print(fruit + " pie")
+#   print(fruit + " pie")
+
+student_grades = [85, 90, 78, 92, 88]
+
+total_exam_score = sum(student_grades)
+print(total_exam_score)
+
+sum = 0
+for grade in student_grades:
+    sum += grade
+print(sum)
+
+# Pick out the top grade
+print(max(student_grades))
+
+top_grade = 0
+for grade in student_grades:
+    if grade > top_grade:
+        top_grade = grade
+print(top_grade)
+
+top_grade = 0
+for grade in student_grades:
+    top_grade = max(grade, top_grade)
+print(top_grade)
