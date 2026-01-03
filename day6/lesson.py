453726184dad2ac9a0748e692593c6ba08a0c45e
Edina Ilyes <edina.ilyes@LEGO.com>
2026-01-03 21:56:44 +0100

    functions

---
+++ day6/lesson.py
@@ -0,0 +1,12 @@
+# existing functions in Python
+print("Hello")
+num_char = len("hello")
+print(num_char)
+
+# Create a function
+def greet():
+    print("Hello")
+    print("Bye")
+
+    # Call the function
+    greet()
