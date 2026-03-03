f76980fc581011d0b6c40f0d9b9373feff5c4bd9
Edina Ilyes <edina.ilyes@LEGO.com>
2026-03-03 11:38:53 +0100

    encrypt/decrypted

--- day8/main.py
+++ day8/main.py
@@ -1,37 +1,45 @@
 # Encode and decode messages
+from caesar_art import logo
+print(logo)
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
-description = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
 
 # TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs and returns the encrypted text.
 # TODO-2: Call the encrypt function and pass the user inputs. Print the encrypted text.
 # TODO-3: Create a function called 'decrypt' that takes the 'text' and 'shift' as inputs and returns the decrypted text.
 # TODO-4: Call the decrypt function and pass the user inputs. Print the decrypted text.
-#Hello 2
-def encrypt(original_text, shift_amount):
-    cipher_text = ""
 
-    for latter in original_text:
-        shifted_position = alphabet.index(latter) + shift_amount # 7 -> 9
-        shifted_position %= len(alphabet) # we are always in the range of 0-25
-        cipher_text += alphabet[shifted_position] # H -> J
 
-    print(f"Here is the encoded result {cipher_text}")
+def caesar(start_text, shift_amount, encode_or_decode):
+    output_text = ""
 
-encrypt(original_text=text, shift_amount=shift)
+    shift = shift_amount
+    if encode_or_decode == "decode":
+        shift *= -1
 
-def decrypt(cipher_text, shift_amount):
-    original_text = ""
+    for letter in start_text:
 
-    for latter in cipher_text:
-        shifted_position = alphabet.index(latter) - shift_amount # 9 -> 7
-        shifted_position %= len(alphabet) # we are always in the range of 0-25
-        original_text += alphabet[shifted_position] # J -> H
+        if letter not in alphabet:
+            output_text += letter
+        else:
 
-    print(f"Here is the decoded result {original_text}")
+            shifted_position = alphabet.index(letter) + shift
+            shifted_position %= len(alphabet)
+            output_text += alphabet[shifted_position]
 
-decrypt(cipher_text=text, shift_amount=shift)
+    print(f"Here's the {encode_or_decode}d result: {output_text}")
+
+should_continue = True
+while should_continue:
+    description = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    caesar(start_text=text, shift_amount=shift, encode_or_decode=description)
+
+    restart = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n").lower()
+    if restart == "no":
+        should_continue = False
+        print("Goodbye")
+
 
